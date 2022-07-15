1d96efae5647ab5df906dab82f63641c72920d0b
TrevorTress <trevor.tress@outlook.com>
2022-07-14 21:58:46 -0400

    test update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>
-        This is a WIP React Portfolio
+        This is Trevor Tress' WIP React Portfolio
       </h1>
 
       <a href="https://thoughtboxer.herokuapp.com/">Thought Boxer</a>
